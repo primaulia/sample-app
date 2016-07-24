d0470b00e8d6be26f3f34ecafdfb045c055bd344
Prima Aulia Gusta <primaulia@gmail.com>
2016-07-25 01:52:41 +0800

    basic login and logout structure, still require fencing for different user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :show, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -6,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App #{ @user.name }!"
       redirect_to @user
     else
@@ -18,10 +21,23 @@ class UsersController < ApplicationController
     # debugger
   end
 
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
